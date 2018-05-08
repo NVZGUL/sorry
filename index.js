5698ff46e580714ecbb668f52cb00f48c7ca2493
NVZGUL <h3rmess@gmail.com>
2018-05-08 23:58:55 +0300

    change port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.Port || 3001
+
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
     res.sendfile(path.join(__dirname, 'static', 'index.html'));
 });
 
-app.listen(3001);
+app.listen(port);
